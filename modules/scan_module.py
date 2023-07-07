5dcdc925040d34a250e9fe5141aa42ce06480c5f
rizumiya <zeinforrell69@gmail.com>
2023-07-07 10:52:26 +0700

    add fixed version of scanning

--- modules/scan_module.py
+++ modules/scan_module.py
@@ -17,28 +17,32 @@ class scan_module:
 
         # variable setting
         self.idLogin = setting[1]
-        self.selectedCamera = setting[2]
+        self.camera_number = setting[2]
         self.selectedSub = setting[3]
-        self.showAnswer = setting[4]
+        self.show_answer = setting[4]
         self.autoSave = setting[5]
 
         # variable subject
-        self.bykTanya = detailSub[2]
-        self.bykJawab = detailSub[3]
-        self.jawaban = jawaban
+        self.question = detailSub[2]
+        self.choice = detailSub[3] + 1
+        self.ansid = 0
+        self.ans = jawaban
+        self.box_pilgan = (self.question // 10) 
 
         self.webcam_on = True
+        self.imgPath = "p.jpg"
         now = datetime.now()
         self.waktu = now.strftime("%Y-%m-%d %H:%M")
-        self.widthImg = 800
-        self.heightImg = 600
+        self.widthImg = 600
+        self.heightImg = 800
         self.kernel = np.ones((5, 5), np.uint8)
 
         # Inisialisasi video webcam
-        self.cap = cv2.VideoCapture(self.selectedCamera)
-        self.cap.set(10, 150)
-        self.threshold_value = 140
+        self.cap = cv2.VideoCapture(self.camera_number)
+        self.threshold_value = 95
         self.create_threshold()
+
+
 
 
     # Membuat fungsi untuk update threshold
@@ -55,30 +59,220 @@ class scan_module:
                            self.update_threshold)
 
 
+    def on_threshold_changed(self, value):
+        self.threshold_value = value
+    
+
+    def splitBoxes(self, img):
+        rows = np.vsplit(img, 10)
+        boxes = []
+        for r in rows:
+            cols = np.hsplit(r, 5)
+            for box in cols:
+                boxes.append(box)
+        return boxes
+
+
+    def check_exam_paper(self, valid_contours):
+        if len(valid_contours) == 0:
+            cv2.putText(self.imgFinal, "No Exam Paper Detected!", (85, 400),
+                            cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 225), 3)
+
+
+    def kotak_contour(self, dilated_edges):
+        contours, _ = cv2.findContours(dilated_edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+        valid_contours = []
+        for contour in contours:
+            perimeter = cv2.arcLength(contour, True)
+            approx = cv2.approxPolyDP(contour, 0.02 * perimeter, True)
+            if len(approx) == 4 and cv2.contourArea(approx) > 5000:
+                valid_contours.append(approx)
+        
+        valid_contours = valid_contours[:4]
+
+        valid_contours = sorted(valid_contours, key=lambda x: cv2.boundingRect(x)[0])
+
+        self.check_exam_paper(valid_contours)
+
+        return valid_contours[:self.box_pilgan]
+    
+
     def preprocessing(self, frame):
         frame = cv2.resize(frame, (self.widthImg, self.heightImg))
-        imgContourTerbesar = frame.copy()
-        imgContours = frame.copy()
-        imgFinal = frame.copy()
         imgGray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        imgBlur = cv2.GaussianBlur(imgGray, (5, 5), 1)
-        imgCanny = cv2.Canny(imgBlur, 10, 40)
-        imgDilate = cv2.dilate(imgCanny, self.kernel, iterations=1)
+        imgGray = cv2.bilateralFilter(imgGray, 11, 17, 17)
+
+        # Menggunakan dilasi untuk memperbesar ketebalan garis tepi
+        kernel = np.ones((2, 2), np.uint8)
+        dilated_edges = cv2.Canny(imgGray, self.threshold_value, self.threshold_value * 2)
+        dilated_edges = cv2.dilate(dilated_edges, kernel, iterations=1)
+
+        self.imgFinal = frame.copy()
+        valid_contours = self.kotak_contour(dilated_edges)
+
+        return frame, valid_contours
+    
+
+    def order_points(self, pts):
+        rect = np.zeros((4, 2), dtype="float32")
+        s = pts.sum(axis=1)
+        rect[0] = pts[np.argmin(s)]
+        rect[2] = pts[np.argmax(s)]
+        diff = np.diff(pts, axis=1)
+        rect[1] = pts[np.argmin(diff)]
+        rect[3] = pts[np.argmax(diff)]
+
+        return rect
+    
+
+    def warp_prespective(self, image, pts):
+        rect = self.order_points(pts)
+
+        dst = np.array([
+            [0, 0], 
+            [self.widthImg, 0], 
+            [self.widthImg, self.heightImg], 
+            [0, self.heightImg]], dtype="float32")
+        
+        M = cv2.getPerspectiveTransform(rect, dst)
+        warped = cv2.warpPerspective(image, M, (self.widthImg, self.heightImg))
+
+        self.ttk1 = rect
+        self.ttk2 = dst
+
+        return warped
+
+
+    def show_answers(self, img, myIndex, grading, ans, questions, choices, ansid):
+        secW = int(img.shape[1]/choices)
+        secH = int(img.shape[0]/questions)
+
+        for x in range(0, questions):
+            myAns = myIndex[x]
+            cX = (myAns * secW) + secW // 2
+            cY = (x * secH) + secH // 2
+            if grading[x] == 1:
+                myColor = (0, 255, 0)
+                cv2.rectangle(img, (myAns*secW, x*secH), ((myAns*secW) +
+                            secW, (x*secH)+secH), myColor, cv2.FILLED)
+                cv2.circle(img, (cX, cY), 35, myColor, cv2.FILLED)
+            else:
+                myColor = (0, 0, 255)
+                cv2.rectangle(img, (myAns * secW, x * secH), ((myAns *
+                            secW) + secW, (x * secH) + secH), myColor, cv2.FILLED)
+                cv2.circle(img, (cX, cY), 35, myColor, cv2.FILLED)
+
+                # Menampilkan jawaban benar dengan lingkaran
+                myColor = (0, 255, 0)
+                correctAns = ans[ansid][x]
+                cv2.circle(img, ((correctAns * secW)+secW//2, (x * secH) + secH // 2),
+                        20, myColor, cv2.FILLED)
+
+
+    def get_student_answer(self, boxes):
+        jawabanPixelVal = np.zeros((10, self.choice))
+        hitC = 0
+        hitR = 0
+
+        for pilgan in boxes:
+            if hitC == 0:
+                jawabanPixelVal[hitR][hitC] = 0
+                hitC += 1
+                continue
+
+            totalPixels = cv2.countNonZero(pilgan)
+            jawabanPixelVal[hitR][hitC] = totalPixels
+
+            hitC += 1
+            if (hitC == self.choice):
+                hitR += 1
+                hitC = 0
+
+        return jawabanPixelVal
+
+    
+    def check_answer(self, boxes):
+        jawabanPixelVal = self.get_student_answer(boxes)
+
+        self.jawabanIndex = []
+        for x in range(0, 10):
+            arr = jawabanPixelVal[x]
+            nilaiJawabanIndex = np.where(arr == np.amax(arr))
+            self.jawabanIndex.append(nilaiJawabanIndex[0][0])
+
+        # Evaluate the answers
+        penilaian = []
+        salah = []
+        for x in range(0, 10):
+            if self.ans[self.ansid][x] == self.jawabanIndex[x]:
+                penilaian.append(1)
+            else:
+                penilaian.append(0)
+                salah.append(x + 1)
+
+        jawab_benar = sum(penilaian)  # 1 nilai
+
+        return jawab_benar, penilaian
 
 
     def start_scanning(self):
-
         while True:
             if self.webcam_on:
                 _, frame = self.cap.read()
+            else:
+                frame = cv2.imread(self.imgPath)
+
+            frame, valid_contours = self.preprocessing(frame)
+            imgCopy = frame.copy()
+
+            total_score = 0
+            jwb_benar = 0
+
+            # Menggambar kotak untuk setiap kontur
+            for i, boxpilgan in enumerate(valid_contours):
+                # Ambil titik sudut
+                x, y, w, h = cv2.boundingRect(boxpilgan)
+
+                cv2.drawContours(imgCopy, [boxpilgan], -1, (0, 255, 0), 2)
+
+                warped = self.warp_prespective(frame, boxpilgan.reshape(4, 2))
+
+                imgWrapGray = cv2.cvtColor(warped, cv2.COLOR_BGR2GRAY)
+                imgTresh = cv2.threshold(imgWrapGray, 140, 255,
+                                         cv2.THRESH_BINARY_INV)[1]
+
+                # Bagi tiap kotak menjadi perkotak
+                boxes = self.splitBoxes(imgTresh)
+                cv2.imshow(f"test{i}", boxes[0])
+                self.ansid = i
+
+                jawaban_benar, penilaian = self.check_answer(boxes)
+                cv2.putText(imgCopy, str(jawaban_benar), (x, y-10),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2)
+                
+                jwb_benar += jawaban_benar
+
+                imgWarpMentah = np.zeros_like(warped)
+
+                self.show_answers(imgWarpMentah, self.jawabanIndex, penilaian, self.ans, 10, self.choice, self.ansid)
+
+                if self.show_answer:
+                    invMatrix = cv2.getPerspectiveTransform(self.ttk2, self.ttk1)
+                    imgInWarp = cv2.warpPerspective(imgWarpMentah, invMatrix,(self.widthImg, self.heightImg))
+
+                    self.imgFinal = cv2.addWeighted(self.imgFinal, 1, imgInWarp, 1, 0)
+                    
+            # Perhitungan total score
+            total_score = (jwb_benar / self.question) * 100
+
+            cv2.putText(self.imgFinal, str(int(total_score)), (20, 80),
+                        cv2.FONT_HERSHEY_SIMPLEX, 3, (0, 255, 0), 3)
             
-            cv2.imshow("OMRay | Scanning", frame)
-            self.preprocessing(frame)
+            cv2.imshow("OMRay | Scanning", self.imgFinal)
 
-            key = cv2.waitKey(1)
-
-            # q untuk berhenti
-            if key == ord("q"):
+            # Menghentikan program jika tombol 'q' ditekan
+            if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
 
         self.cap.release()
